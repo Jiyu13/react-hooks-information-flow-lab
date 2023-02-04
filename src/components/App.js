bf093d3e477015b4164aa53cae1970132436ffbd
Ziru Zou <jiyucoding@yahoo.com>
2023-02-04 13:16:55 -0500

    pass down state and callback that updates state to Header

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
+import Header from "./Header"
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -11,12 +12,10 @@ function App() {
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={onDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
+      
+      {/* Deliverable 1: pass down state and callback that updates state to Header */}
+      <Header onChangeMode={onDarkModeClick} isDarkMode={isDarkMode}/>
+      
       <ShoppingList items={itemData} />
     </div>
   );
